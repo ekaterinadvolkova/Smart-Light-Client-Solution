e2c94ab22cc2fa3775e0b513207d3ec7e954dd26
Ekaterina Volkova <ekaterina.volkova@zalando.fi>
2022-11-23 10:48:08 +0200

    Initial Commit

---
+++ Smart-Light-Client-Solution/Smart_Light_Client_SolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_Light_Client_SolutionApp.swift
+//  Smart-Light-Client-Solution
+//
+//  Created by Ekaterina Volkova on 23.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Smart_Light_Client_SolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
