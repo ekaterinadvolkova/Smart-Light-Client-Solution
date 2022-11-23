19937fb04eba07b96439589f8a1b5a98a85d4d5d
Ekaterina Volkova <ekaterina.volkova@zalando.fi>
2022-11-23 10:56:59 +0200

    commit content view

--- Smart-Light-Client-Solution/ContentView.swift
+++ Smart-Light-Client-Solution/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tabSelection = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        TabView(selection: $tabSelection) {
+            
+            HomeView(tabSelection: $tabSelection)
+                .tabItem {
+                    Image(systemName: "house.fill")
+                    Text("Home")
+                }
+                .tag(1)
+            
+            DiscoverView(tabSelection: $tabSelection)
+                .tabItem {
+                    Image(systemName: "location.circle")
+                    Text("Discover")
+                }
+                .tag(2)
+            
+            FAQView(tabSelection: $tabSelection)
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search")
+                }
+                .tag(3)
+            
+            ProfileView(tabSelection: $tabSelection)
+                .tabItem {
+                    Image(systemName: "person.circle")
+                    Text("Me")
+                }
+                .tag(4)
+            
         }
-        .padding()
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
